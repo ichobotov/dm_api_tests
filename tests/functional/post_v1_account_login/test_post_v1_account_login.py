1f2b2db95e6de365c437d79ca7d6e76df0b54d5a
ichobotov <ivan44@bk.ru>
2025-01-14 14:07:10 +0300

    написали сценарий для запуска тестов

---
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -0,0 +1,75 @@
+import time
+
+from json import loads
+
+from dm_api_account.apis.account_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
+from mailhog_api.apis.mailhog_api import MailhogApi
+
+
+def test_post_v1_account_login():
+    # Регистрация пользователя
+    account_api = AccountApi(host='http://5.63.153.31:5051')
+    login_api = LoginApi(host='http://5.63.153.31:5051')
+    mailhog_api =MailhogApi(host='http://5.63.153.31:5025')
+    login = 'ivan_16'
+    email = f'{login}@mail.ru'
+    password = '123456789'
+
+    json_data = {
+        'login': login,
+        'email': email,
+        'password': password,
+    }
+
+    response = account_api.post_v1_account(json_data=json_data)
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 201, f"Пользователь не был создан, {response.json()}"
+
+    # Получение писем и почтового ящика
+    time.sleep(0.5)  # Введение задержки, тк иногда возникает ситуация, когда в response еще нет письма с нужным логином
+    response = mailhog_api.get_api_v2_messages()
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 200, "Письма не получены"
+
+    # Получение активационного токена
+    token = get_activation_token_by_login(login, response)
+    print(f'Token {token}')
+    assert token is not None, f'токен для пользователя {login} не был получен'
+
+    # Активация пользователя
+    response = account_api.put_v1_account_token(token=token)
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 200, "Пользователь не был активирован"
+
+    # Авторизация
+    json_data = {
+        'login': login,
+        'password': password,
+        'rememberMe': True,
+    }
+
+    response = login_api.post_v1_account_login(json_data=json_data)
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 200, "Пользователь не был авторизован"
+
+def get_activation_token_by_login(login, response):
+    token = None
+    for item in response.json()['items']:
+
+        user_data = loads(item['Content']['Body'])
+        user_login = user_data['Login']
+        if user_login == login:
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+            break
+    return token
+
+
+
+
+
+
