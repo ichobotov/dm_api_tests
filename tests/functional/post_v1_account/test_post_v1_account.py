ea59713941479fa7548b77b322c415049ec8e50f
ichobotov <ivan44@bk.ru>
2025-01-18 19:12:29 +0300

    добавили curl

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,10 +1,17 @@
 import time
-
 from json import loads
+import structlog
 
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from mailhog_api.apis.mailhog_api import MailhogApi
+
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(indent=4, ensure_ascii=True, sort_keys=True)
+    ]
+
+)
 
 
 def test_post_v1_account():
@@ -12,7 +19,7 @@ def test_post_v1_account():
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api =MailhogApi(host='http://5.63.153.31:5025')
-    login = 'ivan_20'
+    login = 'ivan_32'
     email = f'{login}@mail.ru'
     password = '123456789'
 
@@ -23,26 +30,19 @@ def test_post_v1_account():
     }
 
     response = account_api.post_v1_account(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 201, f"Пользователь не был создан, {response.json()}"
 
     # Получение писем и почтового ящика
     time.sleep(0.5)  # Введение задержки, тк иногда возникает ситуация, когда в response еще нет письма с нужным логином
     response = mailhog_api.get_api_v2_messages()
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Письма не получены"
 
     # Получение активационного токена
     token = get_activation_token_by_login(login, response)
-    print(f'Token {token}')
     assert token is not None, f'токен для пользователя {login} не был получен'
 
     # Активация пользователя
     response = account_api.put_v1_account_token(token=token)
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Пользователь не был активирован"
 
     # Авторизация
@@ -53,8 +53,6 @@ def test_post_v1_account():
     }
 
     response = login_api.post_v1_account_login(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Пользователь не был авторизован"
 
 def get_activation_token_by_login(login, response):
