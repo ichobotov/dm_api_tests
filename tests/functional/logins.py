46c17cc02ca6d03e42745d082b68b569c9d7c73e
ichobotov <ivan44@bk.ru>
2025-01-18 22:23:15 +0300

    Добавлен файл с логинами .\tests\functional\logins.py Цель - изменение логинов в одном месте для успешного прохождения тестов

--- tests/functional/logins.py
+++ tests/functional/logins.py
@@ -1,4 +1,4 @@
-post_v1_login_account = 'ivan_54'
-post_v1_login_account_login = 'ivan_55'
-post_v1_login_account_email = 'ivan_56'
-post_v1_login_account_token = 'ivan_57'
+post_v1_login_account = 'ivan_58'
+post_v1_login_account_login = 'ivan_59'
+post_v1_login_account_email = 'ivan_60'
+post_v1_login_account_token = 'ivan_61'
