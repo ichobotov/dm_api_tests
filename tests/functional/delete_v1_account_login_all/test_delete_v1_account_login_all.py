01d0c2f65546df7daf878f094cf1c7d352fc7d3d
ichobotov <ivan44@bk.ru>
2025-01-19 20:38:59 +0300

    Добавлены тесты на logout

--- tests/functional/delete_v1_account_login_all/test_delete_v1_account_login_all.py
+++ tests/functional/delete_v1_account_login_all/test_delete_v1_account_login_all.py
@@ -1,3 +1,3 @@
-def test_delete_v1_account_login(auth_account_helper):
+def test_delete_v1_account_login_all(auth_account_helper):
     response = auth_account_helper.dm_api_account.login_api.delete_v1_account_login_all()
     assert response.status_code == 204, "Пользователь не был разлогирован cо всех устройств"
