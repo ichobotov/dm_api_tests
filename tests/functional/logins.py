0d6552327f60529d16023ff1cee4689e498b9930
ichobotov <ivan44@bk.ru>
2025-01-19 16:25:49 +0300

    добавлен декоратор ожидания

--- tests/functional/logins.py
+++ tests/functional/logins.py
@@ -1,4 +1,4 @@
-post_v1_login_account = 'ivan_58'
-post_v1_login_account_login = 'ivan_59'
-post_v1_login_account_email = 'ivan_60'
-post_v1_login_account_token = 'ivan_61'
+post_v1_login_account = 'ivan_66'
+post_v1_login_account_login = 'ivan_67'
+post_v1_login_account_email = 'ivan_68'
+post_v1_login_account_token = 'ivan_69'
