a5c2a0d535f8d1c3e711c24c3aa2acbed860067d
ichobotov <ivan44@bk.ru>
2025-01-25 20:40:45 +0300

    Добавил Pyhamcrest проверки для get_v1_account

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,6 +1,45 @@
-def test_get_v1_account_auth(auth_account_helper):
-    auth_account_helper.dm_api_account.account_api.get_v1_account()
+from datetime import datetime
 
-def test_get_v1_account_no_auth(account_helper):
-    account_helper.dm_api_account.account_api.get_v1_account()
+from hamcrest import (
+    assert_that,
+    has_property,
+    all_of,
+    instance_of,
+    has_properties,
+    equal_to,
+    contains_inanyorder,
+)
+
+
+def test_get_v1_account_auth(
+        auth_account_helper
+    ):
+    response = auth_account_helper.dm_api_account.account_api.get_v1_account()
+    assert_that(
+        response, all_of(
+            has_property(
+                'resource', has_properties(
+                    'login', equal_to("ivan_65"),
+                    'online', instance_of(datetime),
+                    'rating', has_properties(
+                        'enabled', equal_to(True),
+                        'quality', equal_to(0),
+                        'quantity', equal_to(0)
+                    ),
+                    'registration', instance_of(datetime),
+                    'roles', contains_inanyorder("Guest", "Player"),
+                )
+
+            )
+
+        )
+    )
+
+
+
+def test_get_v1_account_no_auth(
+        account_helper
+    ):
+    response = account_helper.dm_api_account.account_api.get_v1_account()
+    assert_that(response,has_property('resource', equal_to(None)))
 
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,14 +1,38 @@
-def test_post_v1_account(account_helper, prepare_user):
+from datetime import datetime
+
+from hamcrest import (
+    assert_that,
+    has_property,
+    starts_with,
+    all_of,
+    instance_of,
+    has_properties,
+    equal_to,
+)
+
+
+def test_post_v1_account(
+        account_helper,
+        prepare_user
+):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
     account_helper.register_new_user(login=login, password=password, email=email)
-    account_helper.user_login(login=login, password=password, return_model=False )
-
-
-
-
-
-
-
-
+    response = account_helper.user_login(login=login, password=password, return_model=True)
+    assert_that(
+        response, all_of(
+            has_property('resource', has_property('login', starts_with('ivan'))),
+            has_property('resource', has_property('registration', instance_of(datetime))),
+            has_property(
+                'resource', has_property(
+                    'rating', has_properties(
+                        'enabled', equal_to(True),
+                        "quality", equal_to(0),
+                        "quantity", equal_to(0)
+                    )
+                )
+            )
+        )
+    )
+    print(response)
