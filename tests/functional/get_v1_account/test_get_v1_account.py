59f8c5085edac4119ba2e0843c2e2e577aeb6592
ichobotov <ivan44@bk.ru>
2025-01-26 17:54:34 +0300

    Добавил контекстный менеджер для негативных тестов

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from hamcrest import (
     assert_that,
     has_property,
@@ -10,36 +9,38 @@ from hamcrest import (
     contains_inanyorder,
 )
 
+from checkers.http_checkers import check_status_code_http
+
 
 def test_get_v1_account_auth(
         auth_account_helper
-    ):
-    response = auth_account_helper.dm_api_account.account_api.get_v1_account()
-    assert_that(
-        response, all_of(
-            has_property(
-                'resource', has_properties(
-                    'login', equal_to("ivan_65"),
-                    'online', instance_of(datetime),
-                    'rating', has_properties(
-                        'enabled', equal_to(True),
-                        'quality', equal_to(0),
-                        'quantity', equal_to(0)
-                    ),
-                    'registration', instance_of(datetime),
-                    'roles', contains_inanyorder("Guest", "Player"),
+):
+    with check_status_code_http():
+        response = auth_account_helper.dm_api_account.account_api.get_v1_account()
+        assert_that(
+            response, all_of(
+                has_property(
+                    'resource', has_properties(
+                        'login', equal_to("ivan_65"),
+                        'online', instance_of(datetime),
+                        'rating', has_properties(
+                            'enabled', equal_to(True),
+                            'quality', equal_to(0),
+                            'quantity', equal_to(0)
+                        ),
+                        'registration', instance_of(datetime),
+                        'roles', contains_inanyorder("Guest", "Player"),
+                    )
+
                 )
 
             )
-
         )
-    )
-
 
 
 def test_get_v1_account_no_auth(
         account_helper
-    ):
-    response = account_helper.dm_api_account.account_api.get_v1_account()
-    assert_that(response,has_property('resource', equal_to(None)))
-
+):
+    with check_status_code_http(401, "User must be authenticated"):
+        response = account_helper.dm_api_account.account_api.get_v1_account()
+        assert_that(response, has_property('resource', equal_to(None)))
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import pytest
 from hamcrest import (
     assert_that,
     has_property,
@@ -9,17 +10,20 @@ from hamcrest import (
     has_properties,
     equal_to,
 )
+from checkers.http_checkers import check_status_code_http
 
 
 def test_post_v1_account(
         account_helper,
-        prepare_user
+        prepare_user,
+
 ):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
     account_helper.register_new_user(login=login, password=password, email=email)
     response = account_helper.user_login(login=login, password=password, return_model=True)
+
     assert_that(
         response, all_of(
             has_property('resource', has_property('login', starts_with('ivan'))),
@@ -35,4 +39,29 @@ def test_post_v1_account(
             )
         )
     )
-    print(response)
+
+
+@pytest.mark.parametrize(
+    "login, email, password, expected_code, expected_message, errors_details",
+    [
+        ('A', 'test@mail.ru', '1234567', 400, "Validation failed", {'Login': ['Short']}),
+        ('ivan', 'test_mail.ru', '1234567', 400, "Validation failed", {'Email': ['Invalid']}),
+        ('ivan', 'test@mail.ru', '123', 400, "Validation failed", {'Password': ['Short']}),
+    ]
+)
+def test_post_v1_account_bad_parameters(
+        account_helper,
+        login,
+        email,
+        password,
+        expected_code,
+        expected_message,
+        errors_details,
+
+):
+    with check_status_code_http(expexted_code=expected_code, expected_message=expected_message, errors_details=errors_details):
+        login = login
+        password = password
+        email = email
+        account_helper.register_new_user(login=login, password=password, email=email)
+        account_helper.user_login(login=login, password=password, return_model=True)
