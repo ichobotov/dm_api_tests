9f95c00177dfa3a590e9feeee14bb153b00fbb2a
ichobotov <ivan44@bk.ru>
2025-01-26 19:11:05 +0300

    Добавил checker functions

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,46 +1,18 @@
-from datetime import datetime
-from hamcrest import (
-    assert_that,
-    has_property,
-    all_of,
-    instance_of,
-    has_properties,
-    equal_to,
-    contains_inanyorder,
-)
-
+from checkers.get_v1_account import GetV1Account
 from checkers.http_checkers import check_status_code_http
 
 
 def test_get_v1_account_auth(
         auth_account_helper
-):
+    ):
     with check_status_code_http():
         response = auth_account_helper.dm_api_account.account_api.get_v1_account()
-        assert_that(
-            response, all_of(
-                has_property(
-                    'resource', has_properties(
-                        'login', equal_to("ivan_65"),
-                        'online', instance_of(datetime),
-                        'rating', has_properties(
-                            'enabled', equal_to(True),
-                            'quality', equal_to(0),
-                            'quantity', equal_to(0)
-                        ),
-                        'registration', instance_of(datetime),
-                        'roles', contains_inanyorder("Guest", "Player"),
-                    )
-
-                )
-
-            )
-        )
+        GetV1Account.check_response_values_for_auth_user(response)
 
 
 def test_get_v1_account_no_auth(
         account_helper
-):
+    ):
     with check_status_code_http(401, "User must be authenticated"):
         response = account_helper.dm_api_account.account_api.get_v1_account()
-        assert_that(response, has_property('resource', equal_to(None)))
+        GetV1Account.check_response_values_for_not_auth_user(response)
